dff508012fe6d35cc81d501a3ee9c6d18c4a81a0
Lucky <theluckysingh9897@gmail.com>
2023-04-02 14:18:39 +0530

    Added Dark Mode

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@
 import './App.css';
 import Navbar from './Navbar';
 import Textform from './Textform';
+import React, { Component, useState } from 'react'
+
 
 function App() {
+
+  const [mode,setMode]=useState('light');
+  const toggleMode=()=>{
+    if(mode==='light'){
+    setMode('dark');
+       //document.getElementsByClassName("form-check-label").innerHTML= "Enable Light Mode";
+      // document.getElementsByClassName("form-check-label").style.color= 'Black';
+      document.body.style.backgroundColor='#343a40';
+  }
+    else{
+    setMode('light');
+    document.getElementsByClassName("form-check-label").innerHTML= "Enable Dark Mode";
+    document.body.style.backgroundColor='white';
+  }
+  }
+
   return (
     <>
   
 
-  <Navbar title="TEXTUTILS" About="ABOUT US"></Navbar>
+  <Navbar title="TEXTUTILS" About="ABOUT US"  mode={mode} toggleMode={toggleMode}></Navbar>
   <div className='container my-3 '>
-  <Textform heading="Enter Your Text"></Textform>
+  <Textform heading="Enter Your Text" mode={mode}></Textform>
   </div>
   
   
--- src/Textform.js
+++ src/Textform.js
@@ -32,9 +32,32 @@ export default function Textform(props) {
         setText(s);
         
      }
+     const handleCorrectClick=()=>{
+
+        let s= text.split(/[ ]+/);
+        for(let i=0;i<s.length;i++){
+            s[i]=s[i].charAt(0).toUpperCase()+ s[i].slice(1).toLowerCase();
+        }
+        setText(s.join(" "));
+        
+        
+     }
      const handleToggleClick=()=>{
 
-        console.log(text);
+        let s= text.split(/[ ]+/);
+        
+        for(let i=0;i<s.length;i++){
+            for(let j=0;j<s[i].length;j++){
+                {
+                    if (s[i][j] >= 'A' && s[i][j] <= 'Z')
+                        s[i][j] =  String.fromCharCode(s[i][j].charCodeAt(0) + 'a'.charCodeAt(0) - 'A'.charCodeAt(0));
+                    else if (s[i][j] >= 'a' && s[i][j] <= 'z')
+                        s[i][j] =  String.fromCharCode(s[i][j].charCodeAt(0) + 'A'.charCodeAt(0) - 'a'.charCodeAt(0));
+                }
+            }
+
+        }
+        setText(s.join(" "));
         
         
      }
@@ -46,28 +69,29 @@ export default function Textform(props) {
     const [text,setText]=useState("");
   return (
     <>
-    <div>
+    <div className='container' style={{color:props.mode==='light'?'black':'white'}}>
      
 
-<h1>{props.heading}</h1>
+<h1  >{props.heading}</h1>
          
   <div className="mb-3">
 
-    <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handonChange}></textarea>
+    <textarea className="form-control" style ={{backgroundColor:props.mode==='light'?'white':'grey',color:props.mode==='light'?'black':'white' }} id="exampleFormControlTextarea1" rows="8" value={text} onChange={handonChange}></textarea>
   </div>
   <button className="btn btn-primary" type="submit" onClick={handleupClick}>Convert to Uppercase</button>
   <button className="btn btn-primary mx-3" type="submit" onClick={handlelowerClick}>Convert to Lowercase</button>
-  <button className="btn btn-primary mx-3" type="submit" onClick={handleToggleClick}>Toggle Case</button>
+  <button className="btn btn-primary mx-3" type="submit" onClick={handleCorrectClick}>Correct Case</button>
   <button className="btn btn-primary mx-3" type="submit" onClick={handleClearClick}>Clear</button>
+  <button className="btn btn-primary mx-3" type="submit" onClick={handleToggleClick}>Toggle</button>
     </div>
-    <div className='container my-3'>
+    <div className='container my-3' style={{color:props.mode==='light'?'black':'white'}}>
         <h2>Your Text Summary</h2>
       
         <p>{text.length} characters and {text.split(" ").length} words</p>
         <h3>
             Preview
         </h3>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter Something in the Box"}</p>
     </div>
   </>
   )
